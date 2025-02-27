5c60f0ea3f9b89f959c77ace17d261f6ccc69fd8
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-02-27 17:13:47 -0500

    Update checkout.bash

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -38,4 +38,4 @@ while [[ $# -gt 0 ]]; do
 done
 
 git clone https://github.com/usdot-fhwa-stol/carma-msgs.git "${dir}"/src/CARMAMsgs --branch "${BRANCH}"
-git clone https://github.com/usdot-fhwa-stol/carma-utils.git "${dir}"/src/CARMAUtils --branch arc-175-add-record
+git clone https://github.com/usdot-fhwa-stol/carma-utils.git "${dir}"/src/CARMAUtils --branch "${BRANCH}"
